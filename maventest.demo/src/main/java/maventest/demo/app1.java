ed704bb8ce6c5af9642faf6160f1169dbcff80f3
sshukla <sshukla@SBSCLT45.sbsc.com>
2018-01-16 18:34:19 +0530

    frist commit

---
+++ maventest.demo/src/main/java/maventest/demo/app1.java
@@ -0,0 +1,8 @@
+package maventest.demo;
+
+public class app1 {
+	public String sample(){
+		return "sample";
+	}
+
+}
---
+++ maventest.demo/src/test/java/maventest/demo/apptest.java
@@ -0,0 +1,14 @@
+package maventest.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class apptest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
